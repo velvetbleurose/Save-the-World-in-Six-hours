6da21337c9fbbe816595e22b2a16667e84fd1a98
hlwrocer <duanhanson@gmail.com>
2017-04-30 07:50:50 -0400

    Update
    basic register/login

--- flaskr/app.py
+++ flaskr/app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask import Flask, Response, request, render_template, redirect, url_for
 import flask
-from flaskext.mysql import MySQL
+#from flaskext.mysql import MySQL
 import mongoQ
 
 app = Flask(__name__)
@@ -16,7 +16,18 @@ def login():
     if flask.request.method == 'GET':
         return render_template('login.html')
     elif flask.request.method == 'POST':
-        return
+        #get username account info
+        uid = mongo.getUID({"username":request.form.get("username")})
+        #errorcheck if username exists
+        if uid == None:
+            return render_template('login.html', message='Username not found')
+        #error check if username/password pair is correct
+        #TODO: security
+        uid = mongo.getUID({"username":request.form.get("username"),"password":requests.get.form("password")})
+        if uid == None:
+            return render_template('login.html', message='Incorrect username/password')
+        
+        return render_template('home.html')
 @app.route("/",methods=['GET'])
 def hello():
     return render_template('home.html', register='False')
@@ -34,6 +45,14 @@ def register():
     if flask.request.method == 'GET':
         return render_template('register.html')
     elif flask.request.method == 'POST':
+        username = requests.form.get("username")
+        password = requests.form.get("password")
+        #error check if username already exists within the system.
+        uid = mongo.getUID({"username":request.form.get("username")})
+        if uid == None:
+            return render_template('register.html', message = 'Username already taken')
+        #else create account
+        mongo.createAccount({'username':username,'password':password})
         return render_template('home.html', register='True')
 if __name__ == "__main__":
     app.run(port='5000')
