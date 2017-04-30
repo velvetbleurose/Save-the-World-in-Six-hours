d9e288ea855d03681279af70568bf2fe06d6f7dd
hlwrocer <duanhanson@gmail.com>
2017-04-30 05:06:17 -0400

    Update app.py
    mysql flask stuff, probably change to mongodb after anyways.

--- flaskr/app.py
+++ flaskr/app.py
@@ -1,8 +1,13 @@
 from flask import Flask
 from flask import Flask, Response, request, render_template, redirect, url_for
+from flaskext.mysql import MySQL
 
 app = Flask(__name__)
 
+app.config['MYSQL_DATABASE_USER'] = 'user'
+app.config['MYSQL_DATAHBASE_PASSWORD'] = 'password'
+app.config['MYSQL_DATABSE_DB'] = 'dbname'
+app.config['MYSQL_DATABASE_HOST'] = 'host'
 
 @app.route("/",methods=['GET'])
 def login():
