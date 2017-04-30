6a185cde5b709763b07ebcd6e5f9fd6d0b089df1
hlwrocer <duanhanson@gmail.com>
2017-04-30 04:49:32 -0400

    Add files via upload
    login handler

--- flaskr/app.py
+++ flaskr/app.py
@@ -3,6 +3,12 @@ from flask import Flask, Response, request, render_template, redirect, url_for
 
 app = Flask(__name__)
 
+
+@app.route("/",methods=['GET'])
+def login():
+    if flask.request.method == 'GET':
+        return render_template('login.html')
+
 @app.route("/",methods=['GET'])
 def hello():
     return render_template('home.html')
